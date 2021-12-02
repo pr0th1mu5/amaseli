492d24a4766be29d380554e541db64b69793e796
Clecio Sousa <cleciosousa@ufpi.edu.br>
2021-12-02 14:16:12 -0300

    implementado templates tags para noticias e mostrar elas

--- noticias/views.py
+++ noticias/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Noticias
 
 # Create your views here.
 def noticias(request):
-    return render(request,'noticias.html')
+    pegaNoticias = Noticias.objects.all().order_by('dataNoticia')
+    return render(request,'noticias.html',{'pegaNoticias':pegaNoticias})
 
